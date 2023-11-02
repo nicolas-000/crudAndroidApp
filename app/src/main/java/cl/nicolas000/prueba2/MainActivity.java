e9b424288037dfda0657587e9b60bcd0315336d7
nicolas-000 <nicolas.ayala.a@protonmail.ch>
2023-11-01 21:08:50 -0300

    first

---
+++ app/src/main/java/cl/nicolas000/prueba2/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.nicolas000.prueba2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
